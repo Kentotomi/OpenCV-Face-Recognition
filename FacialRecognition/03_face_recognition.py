f953c65241402a94a5ea26e446e03e039cd99160
Kentomi <tomi029fsnz@gmail.com>
2022-12-06 00:36:20 +0900

    fast commit

--- FacialRecognition/03_face_recognition.py
+++ FacialRecognition/03_face_recognition.py
@@ -11,6 +11,8 @@ Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 import cv2
 import numpy as np
 import os 
+import datetime
+import openpyxl
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
@@ -23,7 +25,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Marcelo', 'Paula', 'Ilza', 'Z', 'W'] 
+names = ['Kento', 'Daiki', 'mask', 'Ilza', 'Z', 'W'] 
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
@@ -34,10 +36,15 @@ cam.set(4, 480) # set video height
 minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
 
+dt_now = datetime.datetime.now()
+
+wb = openpyxl.load_workbook('/home/kentomi/Documents/fr/fr1.xlsx')
+ws = wb['Sheet1']
+i = 1
 while True:
 
     ret, img =cam.read()
-    img = cv2.flip(img, -1) # Flip vertically
+    # img = cv2.flip(img, -1) # Flip vertically
 
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
@@ -64,7 +71,15 @@ while True:
         
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
+        
+        # print(dt_now)
+        ws.cell(i,1,value = dt_now)
+        ws.cell(i,2,value = id)
+        wb.save('/home/kentomi/Documents/fr/fr1.xlsx')
+        i = i + 1
+        
+        
+        
     cv2.imshow('camera',img) 
 
     k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
